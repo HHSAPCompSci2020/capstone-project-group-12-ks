81b50498044dbbaf1d1fef8edd27927cc61a59c6
KG2468 <51798297+KG2468@users.noreply.github.com>
2021-05-07 10:02:30 -0700

    ALPHA BUILD

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,22 @@
+import javax.swing.JFrame;
+
 /**
  * 
  * @author Kinjal
  *
  */
-public class Main {
+public class Main extends JFrame {
+	/**
+	 * Main method creates window
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		System.out.println("Welcome to the Dungeon");
+		Main window = new Main(); 
+		window.setTitle("Legends of Kenjiro: The Infinte Dungeon");
+		window.setBounds(100, 100, 500, 400);
+		window.setResizable(true);
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setVisible(true);
 	}
 }

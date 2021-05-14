4914fc48ff134c976648ee4f68815228e6b9d1ac
KG2468 <51798297+KG2468@users.noreply.github.com>
2021-05-14 10:17:00 -0700

    javadoc for collidable interface

--- src/Collidable.java
+++ src/Collidable.java
@@ -1,8 +1,25 @@
 import java.awt.geom.Rectangle2D;
-
+/**
+ * Interface that all things with collision detection implement
+ * @author kinjal
+ *
+ */
 public interface Collidable {
+	/**
+	 * Runs when collision is detected
+	 * @param other
+	 */
 	public void onImpact(Collidable other);
+	/**
+	 * checks for collision and runs appropriate reaction
+	 * @param other the other object it is checkin for collisions
+	 * @return true or false for whether a collision happened or not
+	 */
 	public boolean collisionCheck(Collidable other);
+	/**
+	 * gets hitbox to use for collision detection
+	 * @return  2d rectangle object for the hitbox
+	 */
 	public Rectangle2D.Double getHitbox();
 	
 }
